ace9b68dad9c50620b02ccff3b7607e4e58167af
Long-LH-3008 <luonghoanglong.3008@gmail.com>
2024-03-16 21:41:25 +0700

    fix nested

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,13 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "index.html"),
-				nested: resolve(__dirname, "pages/index.html"),
+				nested: resolve(__dirname, "nested/src/pages/cart.html"),
+				nested: resolve(__dirname, "nested/src/pages/detailproduct.html"),
+				nested: resolve(__dirname, "nested/src/pages/home.html"),
+				nested: resolve(__dirname, "nested/src/pages/pay.html"),
+				nested: resolve(__dirname, "nested/src/pages/products.html"),
+				nested: resolve(__dirname, "nested/src/pages/signin.html"),
+				nested: resolve(__dirname, "nested/src/pages/signup.html"),
 			},
 		},
 	},
