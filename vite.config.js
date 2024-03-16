e687f6d8dbcfe2605b98ea6d311a754276afe0b3
Long-LH-3008 <luonghoanglong.3008@gmail.com>
2024-03-16 22:05:37 +0700

    index

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, "./pages/index.html"),
+				main: resolve(__dirname, "index.html"),
 				pay: resolve(__dirname, "./pages/pay.html"),
 			},
 		},
